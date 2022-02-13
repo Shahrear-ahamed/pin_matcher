cab507cebdb347e0615cabbc8343c171fc66c594
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-02-13 20:15:06 +0600

    add calculation and matching pins

--- js/app.js
+++ js/app.js
@@ -8,3 +8,35 @@ function getPin(){
         getPin();
     }
 }
+// typed_number
+// key_pad
+
+document.getElementById("key_pad").addEventListener("click", function(e){
+    const numbers = document.getElementById("typed_number")
+    const eventValue = e.target.innerText;
+    if(isNaN(eventValue)){
+        if(eventValue == "C"){
+            numbers.value = ""
+        }
+    }else{
+        numbers.value = numbers.value+eventValue;
+    }
+});
+
+function submitPin(){
+    const generatedPin = document.getElementById("pin_value");
+    const inputPin = document.getElementById("typed_number");
+
+    const success = document.getElementById("success");
+    const error = document.getElementById("errors");
+    if(generatedPin.value == inputPin.value){
+        success.style.display = "block";
+        error.style.display = "none";
+        generatedPin.value = "";
+        inputPin.value = "";
+    }else{
+        success.style.display = "none";
+        error.style.display = "block";
+        inputPin.value = "";
+    }
+}
